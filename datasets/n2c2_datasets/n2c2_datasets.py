ee285cb88a068c771fc1afca93de7b14f0e8f257
Alissa Valentine <valentine.alissa@gmail.com>
2022-02-23 17:41:35 -0500

    adding new dataset configuration

--- datasets/n2c2_datasets/n2c2_datasets.py
+++ datasets/n2c2_datasets/n2c2_datasets.py
@@ -22,7 +22,9 @@ import datasets
 _DESCRIPTION = """\
 Create n2c2 datasets from different challenges for clinical notes redundancy investigation.
 Configurations: (1) language_model: pretrained ClinicalBert fine-tuning on MLM and NSP tasks;
-(2) smoking_challenge: create dataset to fine-tune ClinicalBERT on the 2006 smoking challenge; 
+(2) smoking_challenge: create dataset to fine-tune ClinicalBERT on the 2006 smoking challenge;
+(3) cohort_selection_challenge: 
+(4) r_language_model: create dataset with redundant synthetic notes to fine-tune ClinicalBERT;
 """
 
 # Link to the official homepage
@@ -31,10 +33,12 @@ _HOMEPAGE = "https://portal.dbmi.hms.harvard.edu/projects/n2c2-nlp/"
 # Uncomment the following line to load the dummy version of the data (e.g., for code debugging)
 # _FOLDER = {'language_model': './datasets/n2c2_datasets/dummy/language_model/0.0.1/dummy_data',
 #            'smoking_challenge': './datasets/2006_smoking_status/dummy/smoking_challenge/0.0.1/dummy_data',
-#            'cohort_selection_challenge': './datasets/2018_cohort_selection/dummy/cohort_selection_challenge/0.0.1/dummy_data'}
+#            'cohort_selection_challenge': './datasets/2018_cohort_selection/dummy/cohort_selection_challenge/0.0.1/dummy_data',
+#            'r_language_model': './datasets/n2c2_datasets/synthetic_n2c2_datasets/dummy/r_language_model/0.0.1/dummy_data'}
 _FOLDER = {'language_model': './datasets/n2c2_datasets',
            'smoking_challenge': './datasets/2006_smoking_status',
-           'cohort_selection_challenge': './datasets/2018_cohort_selection'}
+           'cohort_selection_challenge': './datasets/2018_cohort_selection',
+           'r_language_model': './datasets/n2c2_datasets/synthetic_n2c2_datasets'}
 
 _SMOKING_LABELS = {'NON-SMOKER': 0,
                    'CURRENT SMOKER': 1,
@@ -58,8 +62,10 @@ class N2c2Dataset(datasets.GeneratorBasedBuilder):
                                               data_dir='../2006_smoking_status'),
                        datasets.BuilderConfig(name='cohort_selection_challenge', version=VERSION,
                                               description="2018 Task 1 cohort selection",
-                                              data_dir='../2018_cohort_selection')
-                       ]
+                                              data_dir='../2018_cohort_selection'),
+                       datasets.BuilderConfig(name='r_language_model', version=VERSION,
+                                              description="ClinicalBERT fine-tuning redundant dataset",
+                                              data_dir='./synthetic_n2c2_datasets')]
     DEFAULT_CONFIG_NAME = 'language_model'
 
     def _info(self):
@@ -92,6 +98,14 @@ class N2c2Dataset(datasets.GeneratorBasedBuilder):
                                      _COHORT_TAGS}
                 }
             )
+        elif self.config.name == 'r_language_model':
+            features = datasets.Features(
+                {
+                    "sentence": datasets.Value("string"),
+                    "document": datasets.Value("string"),
+                    "challenge": datasets.Value("string")
+                }
+            )
         else:
             features = {}
             pass
@@ -120,7 +134,7 @@ class N2c2Dataset(datasets.GeneratorBasedBuilder):
                 name=datasets.Split.TRAIN,
                 # These kwargs will be passed to _generate_examples
                 gen_kwargs={
-                    "filepath": os.path.join(data_dir, 'train_sentences.txt'),
+                    "filepath": os.path.join(data_dir, 'train_sentences.txt'),  # needs to be updated to synthetic
                     "split": "train",
                 },
             ),
@@ -167,6 +181,13 @@ class N2c2Dataset(datasets.GeneratorBasedBuilder):
                             "id": str(row[0]),
                             "label_MET": tag_lab,
                             "label_NOTMET": tag_lab
+                        }
+                elif self.config.name == 'r_language_model':
+                    if len(row) > 0:
+                        yield id_, {
+                            "sentence": row[-1],
+                            "document": str(row[0]),
+                            "challenge": str(row[1]),
                         }
                 else:
                     pass
