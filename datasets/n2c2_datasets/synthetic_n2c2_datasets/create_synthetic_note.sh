601243a3ecf47d3aac38d60c3c0d84c8238e120b
Alissa Valentine <valentine.alissa@gmail.com>
2022-03-25 13:25:09 -0400

    editing README and bash script for synthetic notes

--- datasets/n2c2_datasets/synthetic_n2c2_datasets/create_synthetic_note.sh
+++ datasets/n2c2_datasets/synthetic_n2c2_datasets/create_synthetic_note.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 DATA_DIR=../synthetic_n2c2_datasets
+OUTPUT_DIR=./synthetic_notes
 TRAIN_FILE=train_sentences.txt
-OUTPUT_FILE=synthetic_notes.txt
-OUTPUT_WORDS_FILE=word_metadata.txt
-OUTPUT_SENT_FILE=sentence_metadata.txt
+
+SENT=5 # run this with 5, 10, 15
+WORD=10 # run this with 10, 20, 30
+OUTPUT_FILE=synthetic_notes_$SENT$WORD.txt
+OUTPUT_WORDS_FILE=word_metadata_$SENT$WORD.txt
+OUTPUT_SENT_FILE=sentence_metadata_$SENT$WORD.txt
 
 # This is creating synthetic notes with 3 new sentences and 75% word replacement
 python create_synthetic_note.py \
-  --s=3 \
-  --w=75 \
+  --s=$SENT \
+  --w=$WORD \
   --input_file=$DATA_DIR/$TRAIN_FILE \
-  --output_file=$OUTPUT_FILE \
-  --output_word_metadata_file=$OUTPUT_WORDS_FILE \
-  --output_sent_metadata_file=$OUTPUT_SENT_FILE
+  --output_file=$OUTPUT_DIR/"$OUTPUT_FILE" \
+  --output_word_metadata_file=$OUTPUT_DIR/$OUTPUT_WORDS_FILE \
+  --output_sent_metadata_file=$OUTPUT_DIR/$OUTPUT_SENT_FILE
 
 
 
